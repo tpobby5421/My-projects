43da4602041747db99e741daa06dc064689b0cb8
tpobby5421 <eshun_michael@students.columbusstate.edu>
2025-01-22 13:40:18 -0500

    Add files via upload

---
+++ First.java
@@ -0,0 +1,18 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hola Mundo!");
+    System.out.println("Job title: Data anylyst");
+    System.out.println("Good pay and not high in demand");
+    System.out.println("Median Salary: 84,352");
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
