6dce31c75ded14e8d7d6d82cab19298193b2ee80
tpobby5421 <eshun_michael@students.columbusstate.edu>
2025-02-09 22:20:52 -0500

    Update Pet.java

--- Pet.java
+++ Pet.java
@@ -1,33 +1,72 @@
 public class Pet {   
     // Private data fields
     private String name;
+    private String type;
+    private int age;
 
-    // Public constructor
+    // Default constructor
     public Pet() {
-        this.setName("");
+        this.setName("Random Pet");
+        this.setType("Animal");
+        this.setAge(0);
     }
 
-    // Public mutator method
+    // Custom constructor with 3 parameters
+    public Pet(String name, String type, int age) {
+        this.setName(name);
+        this.setType(type);
+        this.setAge(age);
+    }
+
+    // Set (mutator) methods
     public void setName(String newName) {
         this.name = newName;
     }
 
-    // Public accessor method
+    public void setType(String newType) {
+        this.type = newType;
+    }
+
+    public void setAge(int newAge) {
+        if (newAge >= 0) { // Ensuring age is not negative
+            this.age = newAge;
+        } else {
+            this.age = 0; // Default age if input is invalid
+        }
+    }
+
+    // Get (accessor) methods
     public String getName() {
         return this.name;
     }
 
-    public String toString() {
-        String output = "Pet name: ";
-        output += this.getName();
-        return output;
+    public String getType() {
+        return this.type;
     }
 
-    // Main method
-    public static void main(String[] args) {
-        Pet2 myPet = new Pet2();
-        myPet.setName("Bartholomew");  
-        System.out.println(myPet.getName());  
-        System.out.println("Pet name: " + myPet); 
+    public int getAge() {
+        return this.age;
+    }
+
+    // Method to make the pet speak
+    public String speak() {
+        if (this.type.equalsIgnoreCase("dog")) {
+            return "Woof";
+        } else if (this.type.equalsIgnoreCase("cat")) {
+            return "Meow";
+        } else {
+            return "yowl";
+        }
+    }
+
+    // Updated toString() method
+    public String toString() {
+        String output = "\nPet Information:\n";
+        output += "Type: " + this.getType() + "\n";
+        output += "Name: " + this.getName() + "\n";
+        output += "Sound: " + this.speak() + "\n";
+        output += "Age: " + this.getAge();
+        return output;
     }
 }
+
