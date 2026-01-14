b4ca47fa4ff69c4dd861f8b2701c8da97e642db8
tpobby5421 <eshun_michael@students.columbusstate.edu>
2026-01-14 17:15:52 +0000

    Started with the assignment

---
+++ RestaurantMenu/MenuItem.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class MenuItem{
+    private String name;
+    
+    Scanner input = new Scanner(system.in);
+    system.out.print("Enter Price of item: ");
+    String ItemPrice = input.nextLine();
+
+
+}
